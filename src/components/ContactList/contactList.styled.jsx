efb5d90d9af9c131dec8f402b5c4b4c1a7baf487
KovalchukAnastasia <103355891+KovalchukAnastasia@users.noreply.github.com>
2022-12-03 16:12:57 +0200

    add_hw_02_phonebook

---
+++ src/components/ContactList/contactList.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const List = styled.ul``;
+
+export const ListItem = styled.li`
+  display: flex;
+  margin-bottom: 10px;
+  margin-top: 10px;
+`;
+
+export const ListText = styled.p`
+  margin-right: 10px;
+`;
